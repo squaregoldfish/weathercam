cd3730f81bfa2227ab157c49955e463672244d8a
Steve Jones <steve@squaregoldfish.co.uk>
2021-04-17 17:09:39 +0200

    Timestamp and resize options

--- video/make_video.py
+++ video/make_video.py
@@ -4,32 +4,60 @@ import tempfile
 import contextlib
 import shutil
 from ffmpy import FFmpeg
-
+import toml
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from multiprocessing import Pool
+from itertools import repeat
 """
 Main function
 """
-def main(image_dir):
+def main(config, image_dir):
 
   # Work with a temporary directory
   with make_temp_directory() as tmpdir:
 
     # Get all the jpg files in the folder
-    image_list = (f for f in sorted(os.listdir(image_dir)) if f.endswith('.jpg'))
+    image_list = list(f for f in sorted(os.listdir(image_dir)) if f.endswith('.jpg'))
+    image_indices = range(0, len(image_list))
 
-    last_image = None
-    image_count = 0
-    for img_file in image_list:
-      image_count += 1
-      last_image = img_file
-      shutil.copy(os.path.join(image_dir, img_file), \
-        os.path.join(tmpdir, f'{image_count:05d}.jpg'))
+    pool = Pool()
+    pool.starmap(process_image, \
+      zip(repeat(config), repeat(image_dir), repeat(tmpdir), image_list, image_indices))
+
+    pool.close()
 
     ff = FFmpeg(
+      global_options='-y',
       inputs={f'{os.path.join(tmpdir, "%5d.jpg")}': None},
       outputs={f'{image_dir}.mp4': None}
     )
 
     ff.run()
+
+def process_image(config, indir, outdir, file, index):
+
+  img = Image.open(os.path.join(indir, file))
+  id = ImageDraw.Draw(img)
+
+  if config['commandline']['timestamp']:
+    fillFont = ImageFont.truetype(config['main']['timestamp_font'], 30)
+    shadowFont = ImageFont.truetype(config['main']['timestamp_font'], 30)
+
+    timeText=f'{file[8:10]}:{file[10:12]}'
+
+    xpos = img.size[0] - 117
+    ypos = img.size[1] - 40
+
+    id.text((xpos + 3, ypos + 3), timeText, font=shadowFont, fill=(0, 0, 0))
+    id.text((xpos, ypos), timeText, font=fillFont, fill=(255, 255, 255))
+
+  if config['commandline']['small']:
+    img = img.resize((1280,720))
+
+  img.save(os.path.join(outdir, f'{index:05d}.jpg'))
+
 
 @contextlib.contextmanager
 def make_temp_directory():
@@ -37,12 +65,11 @@ def make_temp_directory():
   try:
     yield temp_dir
   finally:
-    pass
-    #shutil.rmtree(temp_dir)
+    shutil.rmtree(temp_dir)
 
 # Print usage message and quits
 def usage():
-  print('Usage: make_video <image_dir>')
+  print('Usage: make_video [-small] [-timestamp] <image_dir>')
   exit()
 
 # Script run
@@ -55,4 +82,23 @@ if __name__ == '__main__':
     print(f'{image_dir} is not a directory')
     exit()
 
-  main(os.path.dirname(image_dir))
+  config = toml.load('config.toml')
+  config['commandline'] = {
+    'small': False,
+    'timestamp': False
+  }
+
+  argindex = 1
+  while argindex < len(sys.argv) - 1:
+    arg = sys.argv[argindex]
+
+    if arg == '-small':
+      config['commandline']['small'] = True
+      argindex += 1
+    elif arg == '-timestamp':
+      config['commandline']['timestamp'] = True
+      argindex += 1
+    else:
+      usage()
+
+  main(config, os.path.dirname(image_dir))
