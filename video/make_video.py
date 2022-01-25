7954c0e2aae08e69ea8d89f701b5133b1ddfe785
Steve Jones <steve@squaregoldfish.co.uk>
2022-01-25 20:54:42 +0100

    PEP and argparse

--- video/make_video.py
+++ video/make_video.py
@@ -1,157 +1,147 @@
-import sys
-import os
-import tempfile
+import argparse
 import contextlib
+import os
 import shutil
-from ffmpy import FFmpeg
+import tempfile
+from itertools import repeat
+from multiprocessing import Pool
+
 import toml
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-from multiprocessing import Pool
-from itertools import repeat
-"""
-Main function
-"""
-def main(config, image_dir):
+from ffmpy import FFmpeg
 
-  # Work with a temporary directory
-  with make_temp_directory() as tmpdir:
 
-    # Get all the jpg files in the folder
-    start_image = f'{os.path.basename(image_dir)}{config["range"]["start"]}00.jpg'
-    end_image = f'{os.path.basename(image_dir)}{config["range"]["end"]}59.jpg'
+def main(conf, image_dir):
+    """
+    Main function
+    """
 
-    image_list = list(f for f in sorted(os.listdir(image_dir)) \
-      if f >= start_image and f <= end_image)
+    # Work with a temporary directory
+    with make_temp_directory() as tmp_dir:
+        # Get all the jpg files in the folder
+        start_image = f'{os.path.basename(image_dir)}{conf["range"]["start"]}00.jpg'
+        end_image = f'{os.path.basename(image_dir)}{conf["range"]["end"]}59.jpg'
 
-    image_indices = range(0, len(image_list))
+        image_list = list(f for f in sorted(os.listdir(image_dir)) if start_image <= f <= end_image)
 
-    pool = Pool()
-    pool.starmap(process_image, \
-      zip(repeat(config), repeat(image_dir), repeat(tmpdir), image_list, image_indices))
+        image_indices = range(0, len(image_list))
 
-    pool.close()
+        pool = Pool()
+        pool.starmap(process_image, zip(repeat(conf), repeat(image_dir), repeat(tmp_dir), image_list, image_indices))
+        pool.close()
 
-    ff = FFmpeg(
-      global_options='-y',
-      inputs={f'{os.path.join(tmpdir, "%5d.jpg")}': None},
-      outputs={f'{image_dir}.mp4': '-c:v libx264 -profile:v high -pix_fmt yuv420p -g 25 -r 25'}
-    )
+        ff = FFmpeg(
+            global_options='-y',
+            inputs={f'{os.path.join(tmp_dir, "%5d.jpg")}': None},
+            outputs={f'{image_dir}.mp4': '-c:v libx264 -profile:v high -pix_fmt yuv420p -g 25 -r 25'}
+        )
 
-    ff.run()
+        ff.run()
 
-def process_image(config, indir, outdir, file, index):
 
-  try:
-    img = Image.open(os.path.join(indir, file))
-    id = ImageDraw.Draw(img)
+def process_image(conf, in_dir, out_dir, file, index):
+    try:
+        img = Image.open(os.path.join(in_dir, file))
+        img_draw = ImageDraw.Draw(img)
 
-    if config['commandline']['timestamp']:
-      fillFont = ImageFont.truetype(config['main']['timestamp_font'], 30)
-      shadowFont = ImageFont.truetype(config['main']['timestamp_font'], 30)
+        if conf['commandline']['timestamp']:
+            fill_font = ImageFont.truetype(conf['main']['timestamp_font'], 30)
+            shadow_font = ImageFont.truetype(conf['main']['timestamp_font'], 30)
 
-      timeText=f'{file[8:10]}:{file[10:12]}'
+            time_text = f'{file[8:10]}:{file[10:12]}'
 
-      xpos = img.size[0] - 117
-      ypos = img.size[1] - 40
+            x_pos = img.size[0] - 117
+            y_pos = img.size[1] - 40
 
-      id.text((xpos + 3, ypos + 3), timeText, font=shadowFont, fill=(0, 0, 0))
-      id.text((xpos, ypos), timeText, font=fillFont, fill=(255, 255, 255))
+            img_draw.text((x_pos + 3, y_pos + 3), time_text, font=shadow_font, fill=(0, 0, 0))
+            img_draw.text((x_pos, y_pos), time_text, font=fill_font, fill=(255, 255, 255))
 
-    if config['commandline']['small']:
-      img = img.resize((1280,720))
+        if conf['commandline']['small']:
+            img = img.resize((1280, 720))
 
-    img.save(os.path.join(outdir, f'{index:05d}.jpg'))
-  except Exception as e:
-    print(e)
-    pass
+        img.save(os.path.join(out_dir, f'{index:05d}.jpg'))
+    except Exception as e:
+        print(e)
+        pass
 
 
 @contextlib.contextmanager
 def make_temp_directory():
-  temp_dir = tempfile.mkdtemp()
-  try:
-    yield temp_dir
-  finally:
-    shutil.rmtree(temp_dir)
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
+
 
 # Print usage message and quits
 def usage():
-  print('Usage: make_video [-small] [-timestamp] [-start hhmm] [-end hhmm] <image_dir>')
-  exit()
+    print('Usage: make_video [-small] [-timestamp] [-start hhmm] [-end hhmm] <image_dir>')
+    exit()
+
 
 def check_time(hhmm):
+    ok = True
 
-  ok = True
+    if len(hhmm) != 4:
+        ok = False
 
-  if len(hhmm) != 4:
-    ok = False
+    if ok:
+        hours = int(hhmm[0:2])
+        if hours < 0 or hours > 23:
+            ok = False
 
-  if ok:
-    hours = int(hhmm[0:2])
-    if hours < 0 or hours > 23:
-      ok = False
+    if ok:
+        minutes = int(hhmm[2:4])
+        if minutes < 0 or minutes > 59:
+            ok = False
 
+    if not ok:
+        print(f'Invalid time \'{hhmm}\'')
+    return ok
 
-  if ok:
-    minutes = int(hhmm[2:4])
-    if minutes < 0 or minutes > 59:
-      ok = False
-
-  if not ok:
-    print(f'Invalid time \'{hhmm}\'')
-  return ok
 
 # Script run
 if __name__ == '__main__':
-  if len(sys.argv) < 2:
-    usage()
+    config = toml.load('config.toml')
+    config['commandline'] = {
+        'small': False,
+        'timestamp': False
+    }
+    config['range'] = {
+        'start': '0000',
+        'end': '2359'
+    }
 
-  image_dir = sys.argv[-1]
-  if not os.path.isdir(image_dir):
-    print(f'{image_dir} is not a directory')
-    exit()
+    parser = argparse.ArgumentParser(description='Make video from captured images')
+    parser.add_argument('-small', action='store_true', help='Generate a 720p video')
+    parser.add_argument('-timestamp', action='store_true', help='Add a timestamp to the video')
+    parser.add_argument('-start', dest='start', help='Start time (hhmm)')
+    parser.add_argument('-end', dest='end', help='End time (hhmm)')
+    parser.add_argument('img_dir')
 
-  config = toml.load('config.toml')
-  config['commandline'] = {
-    'small': False,
-    'timestamp': False
-  }
-  config['range'] =  {
-    'start': '0000',
-    'end': '2359'
-  }
+    args = parser.parse_args()
+    config['commandline']['small'] = args.small
+    config['commandline']['timestamp'] = args.timestamp
 
-  argindex = 1
-  while argindex < len(sys.argv) - 1:
-    arg = sys.argv[argindex]
+    if args.start is not None:
+        if check_time(args.start):
+            config['range']['start'] = args.start
+        else:
+            print('Invalid start time')
+            exit()
 
-    if arg == '-small':
-      config['commandline']['small'] = True
-      argindex += 1
-    elif arg == '-timestamp':
-      config['commandline']['timestamp'] = True
-      argindex += 1
-    elif arg == '-start':
-      time = sys.argv[argindex + 1]
-      if check_time(time):
-        config['range']['start'] = time
-      else:
-        usage()
-      argindex += 2
-    elif arg == '-end':
-      time = sys.argv[argindex + 1]
-      if check_time(time):
-        config['range']['end'] = time
-      else:
-        usage()
-      argindex += 2
-    else:
-      usage()
+    if args.end is not None:
+        if check_time(args.end):
+            config['range']['end'] = args.end
+        else:
+            print('Invalid end time')
+            exit()
 
-  if config['range']['start'] >= config['range']['end']:
-    print(f'Start must be before end')
-    exit()
+    if config['range']['start'] >= config['range']['end']:
+        print(f'Start must be before end')
+        exit()
 
-  main(config, os.path.dirname(f'{image_dir}/'))
+    main(config, os.path.dirname(f'{args.img_dir}/'))
